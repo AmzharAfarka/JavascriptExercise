3ed444fb4af338bea30f64fc0ba5566fdbbab1bb
Amzhar Afarka <amzhar.afarka@gmail.com>
2025-09-30 20:17:17 +0700

    tugas upd

--- TugasAlgoritma/konversi.js
+++ TugasAlgoritma/konversi.js
@@ -23,31 +23,33 @@ do {
 
     console.log(`${Tahun} Tahun, ${Bulan} Bulan, ${sisaBulan} Hari`);
     
-let hari1 = Number(prompt("Masukkan hari pertama (dd): "));
-let bulan1 = Number(prompt("Masukkan bulan pertama (mm): "));
-let tahun1 = Number(prompt("Masukkan tahun pertama (yy): "));
+    // selisih dua tanggal
+
+    let hari1 = Number(prompt("Masukkan hari pertama (dd): "));
+    let bulan1 = Number(prompt("Masukkan bulan pertama (mm): "));
+    let tahun1 = Number(prompt("Masukkan tahun pertama (yy): "));
 
 
-let hari2 = Number(prompt("Masukkan hari kedua (dd): "));
-let bulan2 = Number(prompt("Masukkan bulan kedua (mm): "));
-let tahun2 = Number(prompt("Masukkan tahun kedua (yy): "));
+    let hari2 = Number(prompt("Masukkan hari kedua (dd): "));
+    let bulan2 = Number(prompt("Masukkan bulan kedua (mm): "));
+    let tahun2 = Number(prompt("Masukkan tahun kedua (yy): "));
 
 
-let total1 = tahun1 * 365 + bulan1 * 30 + hari1;
-let total2 = tahun2 * 365 + bulan2 * 30 + hari2;
+    let total1 = tahun1 * 365 + bulan1 * 30 + hari1;
+    let total2 = tahun2 * 365 + bulan2 * 30 + hari2;
 
 
-let selisih = total2 - total1;
-if (selisih < 0) selisih = -selisih;
+    let selisih = total2 - total1;
+    if (selisih < 0) selisih = -selisih;
 
 
-let tahun = parseInt(selisih / 365);
-let sisahari = selisih % 365;
-let bulan = parseInt(sisa / 30);
-let hari = sisahari % 30;
+    let tahun = parseInt(selisih / 365);
+    let sisahari = selisih % 365;
+    let bulan = parseInt(sisa / 30);
+    let hari = sisahari % 30;
 
 
-console.log(`${tahun} Tahun, ${bulan} Bulan, ${hari} Hari`);
+    console.log(`${tahun} Tahun, ${bulan} Bulan, ${hari} Hari`);
 
 
     jawab = prompt(`diulang (y/n)?`);
